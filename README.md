b41781ca94b5e5180271edaa613a7c40c89441eb
Alan Tesseroli <118698111+AlanTesseroli@users.noreply.github.com>
2023-04-27 20:37:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FORCCE_Website
+ Teste
---
+++ script.js
@@ -0,0 +1,31 @@
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href'); 
+        const targetSection = document.querySelector(targetId); 
+        const offsetTop = targetSection.offsetTop;
+        window.scroll({
+            top: offsetTop,
+            behavior: 'smooth' 
+        });
+    });
+});
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navigations = document.querySelector('nav ul');
+
+hamburgerMenu.addEventListener('click', function() {
+    navigations.classList.toggle('menu-active');
+});
+
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+if (mediaQuery.matches) {
+    navigations.style.display = 'none';
+    hamburgerMenu.style.display = 'block';
+    
+    hamburgerMenu.addEventListener('click', function() {
+        navigations.classList.toggle('menu-active');
+    });
+}
